010f42703838b7b18a807e3830586639b369b6c6
cristian lozano <yatogami2814@gmail.com>
2026-05-27 19:22:23 -0500

    Se crear scafolding

---
+++ backend/Belleza/Belleza/Controllers/Employees/EmployeerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Belleza.Controllers.Employees
+{
+    public class EmployeerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
